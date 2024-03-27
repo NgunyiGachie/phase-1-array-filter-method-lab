09c1156e6d12e6d673af6e09b56a9b39e6a571bd
NgunyiGachie <antogachie@gmail.com>
2024-03-27 15:36:59 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution here
+
+function findMatching(drivers, attribute) {
+    const lowerCaseAttribute = attribute.toLowerCase(); 
+    return drivers.filter(function (driver) { 
+      return driver.toLowerCase() === lowerCaseAttribute; 
+    });
+  }
+  
+function fuzzyMatch(drivers, letters) {
+    const fuzzyDrivers = [];
+
+    for (let i = 0; i < drivers.length; i++) {
+        const firstLetters = drivers[i].slice(0, letters.length).toLowerCase(); 
+
+        if (firstLetters === letters.toLowerCase()) {
+            fuzzyDrivers.push(drivers[i]);
+        }
+    }
+
+    return fuzzyDrivers;
+}
+
+function matchName(drivers, name) {
+    const matchingNames = [];
+
+    for (let i = 0; i < drivers.length; i++) {
+        if (drivers[i].name.toLowerCase() === name.toLowerCase()) {
+            matchingNames.push(drivers[i]);
+        }
+    }
+
+    return matchingNames;
+}
+
+module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+};
